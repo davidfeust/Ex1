5328148ecc66b97adac27c55664fe11ae7533aa0
davidfeust <david.feust@gmail.com>
2020-11-11 17:35:48 +0200

    power for negative numbers

--- power.c
+++ power.c
@@ -7,18 +7,32 @@ double Exp(int x) {
         return 1;
     }
     double res = 1;
-    for (int i = 0; i < x; i++) {
-        res *= EX;
+    if (x > 0) {
+        for (int i = 0; i < x; i++) {
+            res *= EX;
+        }
+        return res;
+    } else {
+        for (int i = 0; i < (x * -1); i++) {
+            res *= EX;
+        }
+        return 1 / res;
     }
-    return res;
 }
 
 double Pow(double x, int y) {
     if (y == 0)
         return 1;
     double res = 1;
-    for (int i = 0; i < y; i++) {
-        res *= x;
+    if (y > 0) {
+        for (int i = 0; i < y; i++) {
+            res *= x;
+        }
+        return res;
+    } else {
+        for (int i = 0; i < (y * -1); i++) {
+            res *= x;
+        }
+        return 1 / res;
     }
-    return res;
 }
